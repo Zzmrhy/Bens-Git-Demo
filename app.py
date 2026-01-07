9d3293cb908406f3054394a19da342460f92f9f3
CMP_BeHedderman <BHedderman07@gmail.com>
2026-01-07 12:49:43 -0500

    message

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 print("Git/Github Demo")
 
-print("Hello from the remote repo")
+print("Hello from the remote repo 0")
 
 
-print("Hello from the local repo")
+print("Hello from the local repo 1")
+
+print("Hey remote, you a dummy!")
