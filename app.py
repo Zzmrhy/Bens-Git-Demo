88b3caf1eb3175c556313ca83cc9f175d89d6264
Benjamin Hedderman <143619414+BenHedderman@users.noreply.github.com>
2026-01-07 12:49:25 -0500

    Update print statements in app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 print("Git/Github Demo")
 
-print("Hello from the remote repo")
+print("Hello from the remote repo 2")
 
 
-print("Hello from the local repo")
+print("Hello from the local repo 3")
+
+print("Hey local, you ain't so tough")
